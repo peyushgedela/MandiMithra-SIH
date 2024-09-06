b003b129a0830d73a05c90c7270d9d51db9d6431
Peyush Gedela <gedela.peyush2022@vitstudent.ac.in>
2024-09-06 18:51:49 +0530

    homepage change

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,22 +1,27 @@
-import { StyleSheet } from "react-native";
-import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from "react";
 import Header from "../../components/Header";
-import { SafeAreaView } from "react-native-safe-area-context";
 import MainNav from "../../components/MainNav";
 
 const Home = () => {
+  const [activeInd, setActiveInd] = useState(0);
+  const backgroundColors = ["#fff8dc", "#DCFFF2"];
+
   return (
-    <SafeAreaView className="flex flex-col" style={styles.container}>
+    <SafeAreaView
+      className="flex-1 flex-col pt-3"
+      style={{ backgroundColor: backgroundColors[activeInd] }}
+    >
       <Header className="basis-1/12" />
-      <MainNav className="basis-2/12" name="buyer" />
+      <MainNav
+        className="basis-1/12"
+        activeInd={activeInd}
+        setActiveInd={setActiveInd}
+      />
     </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff8dc",
-  },
-});
+const styles = StyleSheet.create({});
--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,54 +1,39 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { router } from "expo-router";
 
-const MainNav = (e) => {
-  const { name } = e;
-  var colbuy = "";
-  var colfar = "";
-  var coltbuy = "";
-  var coltfar = "";
-  if (name == "farmer") {
-    colbuy = "#EE4B2B";
-    colfar = "#4CBB17";
-    coltbuy = "#000000";
-    coltfar = "#FFFFFF";
-  } else if (name == "buyer") {
-    colfar = "#EE4B2B";
-    colbuy = "#4CBB17";
-    coltfar = "#000000";
-    coltbuy = "#FFFFFF";
-  }
+const vals = [
+  {
+    name: "Buyer",
+  },
+  {
+    name: "Farmer",
+  },
+];
+
+const MainNav = ({ activeInd, setActiveInd }) => {
+  const selectCategory = (index) => {
+    setActiveInd(index);
+  };
+
   return (
-    <View className="flex flex-row px-4 justify-evenly">
-      <View className="basis-1/3">
-        <TouchableOpacity
-          className="flex items-center justify-center"
-          style={[styles.button, { backgroundColor: colbuy }]}
-          onPress={() => {
-            if (name == "farmer") {
-              router.replace("/home");
-            }
-          }} // Dynamic routing
-        >
-          <Text className="font-mbold text-base" style={{ color: coltbuy }}>
-            Buyer
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View className="basis-1/3">
-        <TouchableOpacity
-          className="flex items-center justify-center"
-          style={[styles.button, { backgroundColor: colfar }]}
-          onPress={() => {
-            if (name == "buyer") router.replace("/farmerhome");
-          }} // Dynamic routing
-        >
-          <Text className="font-mbold text-base" style={{ color: coltfar }}>
-            Farmer
-          </Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container}>
+      {vals.map((val, index) => {
+        return (
+          <TouchableOpacity
+            className="w-1/3 items-center"
+            key={val.name}
+            style={activeInd === index ? styles.buttonClicked : styles.button}
+            onPress={() => selectCategory(index)}
+          >
+            <Text
+              className="font-mbold text-base"
+              style={activeInd === index ? styles.textClicked : styles.text}
+            >
+              {val.name}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
@@ -56,8 +41,24 @@ const MainNav = (e) => {
 export default MainNav;
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  buttonClicked: {
+    backgroundColor: "#4CBB17",
+    padding: 10,
+    borderRadius: 10,
+  },
   button: {
-    padding: 16,
-    borderRadius: 15,
+    backgroundColor: "#EE4B2B",
+    padding: 10,
+    borderRadius: 10,
+  },
+  text: {
+    color: "#000000",
+  },
+  textClicked: {
+    color: "#ffffff",
   },
 });
