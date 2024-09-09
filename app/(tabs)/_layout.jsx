2218066304bed596165b45c33927607676df3e5f
Peyush Gedela <gedela.peyush2022@vitstudent.ac.in>
2024-09-09 22:47:45 +0530

    initialised multi-launguage support

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
-import { Slot, Stack, Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
 const UserTypeLayout = () => {
   return (
---
+++ constants/i18n.js
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources: {
+      en: {
+        translation: {
+          farmer: "Farmer",
+          buyer: "Buyer",
+        },
+      },
+    },
+  });
+
+export default i18n;
